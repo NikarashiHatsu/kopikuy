f6511a465d4a08995e5b4f2212452b3cb453b85d
NikarashiHatsu <yourlovelydev@gmail.com>
2023-05-17 14:41:29 +0700

    add to cart

--- resources/views/livewire/menu.blade.php
+++ resources/views/livewire/menu.blade.php
@@ -1,11 +1,18 @@
-<div>
-    <div class="transition duration-100 ease-in group bg-white hover:shadow-xl cursor-pointer p-4 mb-4 rounded prose-headings:mt-0 prose-headings:uppercase prose-p:mb-0 prose-img:my-0 prose-img:py-0">
+<div class="relative mb-4 overflow-hidden group cursor-pointer">
+    <div class="flex items-center justify-center transition-transform duration-300 ease-in-out w-full bg-amber-600 bg-opacity-80 z-10 h-full rounded absolute top-0 left-0 transform -translate-x-full hover group-hover:translate-x-0">
+        <x-phosphor-shopping-cart-bold class="w-16 h-16 text-white" />
+        <span class="font-display text-5xl text-light ml-6 text-white">
+            Add to cart
+        </span>
+    </div>
+
+    <div class="transition duration-100 ease-in bg-white hover:shadow-xl p-4 rounded prose-headings:mt-0 prose-headings:uppercase prose-p:mb-0 prose-img:my-0 prose-img:py-0">
         <div class="flex items-center">
             <div class="flex-shrink-0">
                 <div class="w-24 h-24 rounded overflow-hidden">
                     <img
                         src="{{ $menu['image'] ?? "" }}"
-                        class="transform-gpu duration-300 ease-in-out w-full h-full bg-gray-300 rounded object-cover group-hover:scale-125 group-hover:-rotate-12"
+                        class="transform-gpu z-0 duration-300 ease-in-out w-full h-full bg-gray-300 rounded object-cover group-hover:scale-125 group-hover:-rotate-12"
                     />
                 </div>
             </div>
