4f1d3db52957a6d4e7b743a3c574d38a3881e8f2
NikarashiHatsu <yourlovelydev@gmail.com>
2023-05-19 21:50:22 +0700

    feat: auth

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,6 +1,10 @@
+@props([
+    'dark' => true,
+])
+
 <div {{ $attributes->merge(['class' => 'flex items-center']) }}>
-    <x-phosphor-coffee class="text-white fill-current w-8 h-8" />
-    <span class="font-display text-white text-2xl ml-2 uppercase">
+    <x-phosphor-coffee @class(["fill-current w-8 h-8 shrink-0", "text-white" => $dark, "text-gray-700" => ! $dark]) />
+    <span @class(["font-display text-2xl ml-2 uppercase", "text-white" => $dark, "text-gray-700" => ! $dark])>
         {{ config('app.name') }}
     </span>
 </div>
